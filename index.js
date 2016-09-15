3de0e0e0bf5bfb3b57797affe118f2e278710760
jimyang <jim.yang@thisplace.com>
2016-09-15 15:55:55 +0200

    Initial working commit, not sure of CA stepping correctness, will check later

---
+++ index.js
@@ -0,0 +1,215 @@
+var world;
+var newWorld;
+var rules;
+
+/**
+ * @param {object}  options
+ * @param {number}  options.w
+ * @param {number}  options.h
+ * @returns {Uint8Array[]}
+ */
+function createWorld(options) {
+    var w = [];
+    for (var i = options.h; i-- > 0;) {
+        var row = new Uint8Array(new ArrayBuffer(options.w));
+        w.push(row);
+    }
+
+    return w;
+}
+
+var worldParams = { w : 64, h : 64 };
+
+world    = createWorld(worldParams);
+newWorld = createWorld(worldParams);
+
+/**
+ * @param {object}          options
+ * @param {number}          options.x
+ * @param {number}          options.y
+ * @param {number}          options.w
+ * @param {number}          options.h
+ * @param {Uint8Array[]}    options.fromWorld
+ * @returns {Uint8Array}
+ */
+function getWorldRect(options) {
+    var x              = options.x;
+    var y              = options.y;
+    var w              = options.w;
+    var h              = options.h;
+    var fromWorld      = options.fromWorld;
+    var flattenedArray = new Uint8Array(w * h);
+    for (var i = 0; i < h; i++) {
+        for (var j = 0; j < w; j++) {
+            var value = 0;
+            if (fromWorld[y + i]) {
+                value = fromWorld[y + i][x + j];
+            }
+
+            flattenedArray[j + i * w] = value;
+        }
+    }
+
+    return flattenedArray;
+}
+
+function randomizeWorld(options) {
+    var h = options.world.length;
+    var w = options.world[0].length;
+
+    for (var i = h; i-- > 0;) {
+        for (var j = w; j-- > 0;) {
+            options.world[i][j] = Math.random() > 0.5 | 0;
+        }
+    }
+}
+
+randomizeWorld({ world : world });
+
+var ctx;
+
+function initGraphics() {
+    if (!ctx) {
+        var h          = world.length;
+        var w          = world[0].length;
+        var c          = document.getElementById('c');
+        c.width        = w;
+        c.height       = h;
+        c.style.width  = w;
+        c.style.height = h;
+        ctx            = c.getContext('2d');
+
+        ctx['imageSmoothingEnabled'] = false;
+    }
+}
+
+/**
+ * @param {object}       options
+ * @param {Uint8Array[]} options.world
+ */
+function drawWorld(options) {
+    initGraphics();
+
+    var h         = options.world.length;
+    var w         = options.world[0].length;
+    var imageData = ctx.createImageData(w, h);
+    for (var i = 0; i < h; i++) {
+        for (var j = 0; j < w; j++) {
+            if (options.world[i][j]) {
+                // set alpha on flattened array
+                var p                 = (i * w + j) * 4;
+                imageData.data[p + 3] = 255;
+            }
+        }
+    }
+
+    ctx.putImageData(imageData, 0, 0, 0, 0, w, h);
+}
+
+drawWorld({ world : world });
+
+/**
+ * @param {object} options
+ * @param {number} options.x
+ * @param {number} options.y
+ * @param {number} options.flattenedArrayRule
+ * @param {number} options.fromWorld
+ * @param {number} options.toWorld
+ */
+function applyRule(options) {
+    var x = options.x;
+    var y = options.y;
+
+    var flattenedArrayRule  = options.flattenedArrayRule;
+    var w                   = Math.sqrt(flattenedArrayRule.length);
+    var flattenedArrayWorld = getWorldRect({
+        fromWorld : options.fromWorld,
+        x         : x - (w >> 1),
+        y         : y - (w >> 1),
+        w         : w,
+        h         : w
+    });
+    var centroidIndex       = flattenedArrayRule.length >> 1;
+    var centroidResult      = flattenedArrayRule[centroidIndex];
+
+    var isMatch = true;
+    for (var i = 0; i < flattenedArrayRule.length; i++) {
+        if (i !== centroidIndex) {
+            if (flattenedArrayWorld[i] !== flattenedArrayRule[i]) {
+                isMatch = false;
+                break;
+            }
+        }
+    }
+
+    if (isMatch) {
+        options.toWorld[y][x] = 1; //centroidResult; //tweakable
+    }
+}
+
+var TWEAKABLE_THRESHOLD_FOR_DARKEN = 0.6;
+
+function getRandom3x3Rule() {
+    var rule = new Uint8Array(9);
+    for (var i = 9; i-- > 0;) {
+        rule[i] = (Math.random() > TWEAKABLE_THRESHOLD_FOR_DARKEN) | 0;
+    }
+    return rule;
+}
+
+/**
+ * @param {object}          options
+ * @param {Uint8Array[]}    options.fromWorld
+ * @param {Uint8Array[]}    options.toWorld
+ */
+function applyAllRules(options) {
+    var h = options.fromWorld.length;
+    var w = options.fromWorld[0].length;
+
+    for (var i = 0; i < h; i++) {
+        for (var j = 0; j < w; j++) {
+            for (var r = 0; r < rules.length; r++) {
+                applyRule({
+                    x                  : j,
+                    y                  : i,
+                    flattenedArrayRule : rules[r],
+                    fromWorld          : world,
+                    toWorld            : newWorld
+                })
+            }
+        }
+    }
+}
+
+/**
+ * Generate n random rules.
+ * @param {number} n
+ * @returns {Uint8Array[]}
+ */
+function getRandomRules(n) {
+    var ruleSet = [];
+    for (; n-- > 0;) {
+        ruleSet.push(getRandom3x3Rule());
+    }
+
+    return ruleSet;
+}
+
+var TWEAKABLE_RULE_COUNT = 20;
+
+rules = getRandomRules(TWEAKABLE_RULE_COUNT);
+
+function stepWorld() {
+    applyAllRules({
+        fromWorld : world,
+        toWorld   : newWorld
+    });
+
+    drawWorld({ world : newWorld });
+
+    world    = newWorld;
+    newWorld = createWorld(worldParams);
+}
+
+// Run automatically
+setInterval(stepWorld, 1);
